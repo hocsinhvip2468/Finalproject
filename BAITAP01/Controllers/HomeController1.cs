746483682179606686e72a5767ace1a91a4141e9
Your Name <you@example.com>
2022-10-25 10:45:19 +0700

    first

---
+++ BAITAP01/Controllers/HomeController1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BAITAP01.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index(double sohang01, double sohang02, string pheptoan)
+        {
+            ViewBag.sohang01 = sohang01;
+            ViewBag.sohang02 = sohang02;
+            ViewBag.pheptoan = pheptoan;
+            double kq = 0;
+            switch (pheptoan)
+            {
+                case "+":
+                    kq = sohang01 + sohang02;
+                    break;
+                case "-":
+                    kq = sohang01 - sohang02;
+                    break;
+                case "*":
+                    kq = sohang01 * sohang02;
+                    break;
+                case "/":
+                    kq = sohang01 / sohang02;
+                    break;
+            }
+            ViewBag.KetQua = kq;
+            return View();
+        }
+    }
+}
